2ab254691d288dc98f5194d9e013d95791c5a4f0
pockemul <pockemul@free.fr>
2014-09-04 17:03:10 +0200

    TI-74

--- src/machine/ti/ti74.cpp
+++ src/machine/ti/ti74.cpp
@@ -366,7 +366,7 @@ quint8 Cti74::getKey()
             if (KEY('3'))			data|=0x02;
             if (KEY('6'))			data|=0x04;
             if (KEY('9'))			data|=0x08;
-            if (KEY(K_POW_OFF))			data|=0x10;
+            if (KEY(K_POW_OFF))		data|=0x10;
             if (KEY('Q'))			data|=0x20;
             if (KEY('A'))			data|=0x40;
 //            if (KEY(''))			data|=0x80;
