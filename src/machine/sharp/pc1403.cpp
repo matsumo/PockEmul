1b05b3778e70d66ee0993f9ef896e41575a1d7a9
pockemul <pockemul@free.fr>
2014-10-29 22:44:00 +0100

    Sharp PC-1403 RAM Mapping
    fixed

--- src/machine/sharp/pc1403.cpp
+++ src/machine/sharp/pc1403.cpp
@@ -104,6 +104,7 @@ bool Cpc1403::Set_Connector(Cbus *_bus)
 
 bool Cpc1403::Get_Connector(Cbus *_bus)
 {
+    Q_UNUSED(bus)
 
     pCPU->Set_Xin(pCONNECTOR->Get_pin(PIN_MT_IN));
 
@@ -156,7 +157,7 @@ bool Cpc1403::Chk_Adr(UINT32 *d,UINT32 data)
     }
 //    if ( (*d>=0x4000) && (*d<=0x7FFF) )	{*d += 0xC000 + (RomBank * 0x4000); return(0); }
     if ( (*d>=0x4000) && (*d<=0x7FFF) )	{*d += memOffset; return(0); }
-    if ( (*d>=0xE000) && (*d<=0xFFFF) )	{ return(1); }
+    if ( (*d>=0x8000) && (*d<=0xFFFF) )	{ *d = *d & 0x1FFF | 0xE000; return(1); }
 
 	return(0);
 
@@ -171,6 +172,7 @@ bool Cpc1403::Chk_Adr_R(UINT32 *d,UINT32 *data)
         *d += memOffset;
         return(1);
     }
+    if ( (*d>=0x8000) && (*d<=0xFFFF) )	{ *d = *d & 0x1FFF | 0xE000; return(1); }
 	return(1);	
 }
 
@@ -203,6 +205,7 @@ bool Cpc1403H::Chk_Adr(UINT32 *d,UINT32 data)
 
 bool Cpc1403H::Chk_Adr_R(UINT32 *d,UINT32 *data)
 {
+    if ( (*d>=0x8000) && (*d<=0xdFFF) )	{ return(1); }
 	return(Cpc1403::Chk_Adr_R(d,data));	
 }
 
